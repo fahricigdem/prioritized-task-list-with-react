c84dbdc97d61f0747fe3eef053ecf31188e1938a
Fahri Cigdem <fahricigdem34@gmail.com>
2021-03-06 03:34:33 +0100

    home design

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Prioritized Task List
-        </p>
-
-        <code style={{ color: "red" }}>Under Construction!</code>
-
-      </header>
+      <div className="TaskGroupRow">
+        <div className="TaskGroup UrgentImp">
+          <h5>Urgent</h5>
+          <h5>Important </h5>
+          <h5><span className="badge rounded-pill bg-danger">0</span></h5>
+        </div>
+        <div className="TaskGroup notUrgentImp">
+          <h5>Important</h5>
+          <h5>not Urgent </h5>
+          <h5><span className="badge rounded-pill bg-secondary">0</span></h5>
+        </div>
+      </div>
+      <div className="AddHome"><h1> + </h1></div>
+      <div className="TaskGroupRow">
+        <div className="TaskGroup UrgentNotImp">
+          <h5>Urgent</h5>
+          <h5>not Important </h5>
+          <h5><span className="badge rounded-pill bg-secondary">0</span></h5></div>
+        <div className="TaskGroup notUrgentNotImp">
+          <h5>not Urgent</h5>
+          <h5>not Important </h5>
+          <h5><span className="badge rounded-pill bg-secondary">0</span></h5>
+        </div>
+      </div>
     </div>
   );
 }
