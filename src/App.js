6b416441c1f86e867a90343a8fe55f3e7730a025
Fahri Cigdem <fahricigdem34@gmail.com>
2021-03-06 03:49:19 +0100

    home

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
         <div className="TaskGroup UrgentImp">
           <h5>Urgent</h5>
           <h5>Important </h5>
-          <h5><span className="badge rounded-pill bg-danger">0</span></h5>
+          <h4><span className="badge rounded-pill bg-danger">0</span></h4>
         </div>
         <div className="TaskGroup notUrgentImp">
           <h5>Important</h5>
           <h5>not Urgent </h5>
-          <h5><span className="badge rounded-pill bg-secondary">0</span></h5>
+          <h4><span className="badge rounded-pill bg-secondary">0</span></h4>
         </div>
       </div>
       <div className="AddHome"><h1> + </h1></div>
@@ -20,11 +20,11 @@ function App() {
         <div className="TaskGroup UrgentNotImp">
           <h5>Urgent</h5>
           <h5>not Important </h5>
-          <h5><span className="badge rounded-pill bg-secondary">0</span></h5></div>
+          <h4><span className="badge rounded-pill bg-secondary">0</span></h4></div>
         <div className="TaskGroup notUrgentNotImp">
           <h5>not Urgent</h5>
           <h5>not Important </h5>
-          <h5><span className="badge rounded-pill bg-secondary">0</span></h5>
+          <h4><span className="badge rounded-pill bg-secondary">0</span></h4>
         </div>
       </div>
     </div>
